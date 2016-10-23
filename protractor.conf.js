8553fe78122f00a9e280f28b0ee616ebeddb93df
Alison Tinker <atinker@vcu.edu>
2016-10-23 16:33:27 -0400

    config

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,7 +3,6 @@ exports.config = {
 
   framework: 'jasmine2',
 
-  chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.24',
 
   capabilities: {
     browserName: 'chrome'
